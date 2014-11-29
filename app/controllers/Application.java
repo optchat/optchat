7c2b7dd42dccb2791216f09cb1b26b163ad116f7
urainkyo <poorian2546@gmail.com>
2014-11-29 15:08:23 +0900

    ログイン画面のレイアウト作成 + 使用しないjsファイル削除

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,12 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.security.login.login;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(login.render());
     }
 
 }
