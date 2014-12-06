9c6239eb299b5a71fd7f8783a8a63c07147fd0c9
s1hit <s1hit77@gmail.com>
2014-12-06 16:12:28 +0900

    ログインしていない場合にログインページに飛ばす制御を追加

--- app/controllers/security/Authentication.java
+++ app/controllers/security/Authentication.java
@@ -25,9 +25,28 @@ public class Authentication extends Controller {
         if (loginForm.hasErrors()) {
             return badRequest(login.render(loginForm));
         }
+        String returnUrl = getReturnUrl();
+
         session().clear();
         session("userId", Long.toString(loginForm.get().user.userId));
-        return redirect(controllers.chat.routes.Channels.connect());
+
+        return redirect(returnUrl);
+    }
+
+    /**
+     * ログイン成功後の遷移先URLを取得する
+     * @return 遷移先URL
+     */
+    private static String getReturnUrl() {
+        // Securityヘルパーで退避した遷移先URLを取得
+        String returnUrl = session("returnUrl");
+        if (returnUrl == null || returnUrl.equals("") ||
+                returnUrl.equals(controllers.chat.routes.Channels.connect().absoluteURL(request()))) {
+
+            // 遷移先URLを取得できない場合は、遷移先をチャットルームに設定
+            returnUrl = controllers.chat.routes.Channels.connect().url();
+        }
+        return returnUrl;
     }
 
 }
